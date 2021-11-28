a50515f3acf6a44c074ae2ad5aae94558306bfdf
Juan <jjjbaque@gmail.com>
2021-11-28 18:18:17 +1100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,131 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+// components
+import  Signup  from './components/Signup'
+import  Signin  from './components/Signin'
+import  { Home }  from './components/Home';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+// firebase
+import { firebaseConfig } from './Config';
+import {initializeApp,} from 'firebase/app'
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth"
+
+import { 
+  initializeFirestore, 
+  getFirestore, 
+  setDoc, 
+  doc, 
+  addDoc, 
+  collection,
+  query, 
+  where, 
+  onSnapshot 
+} from 'firebase/firestore'
+
+
+
+
+initializeApp(firebaseConfig)
+
+
+const Stack = createStackNavigator();
+
+
+
+
+function App() {
+  const[ auth, setAuth ] = useState()
+  const[ user, setUser ] = useState()
+  const [signupError, setSignupError ] = useState()
+  const [signinError, setSigninError ] = useState()
+  const [ data, setData ] = useState()
+
+
+  /*useEffect(() => {
+    onAuthStateChanged( FBauth, (user) => {
+      if( user ) { 
+        setAuth(true) 
+        setUser(user)
+        // console.log( 'authed')
+        if( !data ) { getData() }
+      }
+      else {
+        setAuth(false)
+        setUser(null)
+      }
+    })
+  })*/
+
+  // useEffect( () => {
+  //   if( !data && user ) {
+  //     getData()
+  //   }
+  // }, [data,auth, user])
+
+  const SignupHandler = ( email, password ) => {
+    const auth= getAuth()
+    setSignupError(null)
+    createUserWithEmailAndPassword( auth, email, password )
+    .then( ( userCredential ) => { 
+      setUser(userCredential)
+      setAuth( true )
+    } )
+    .catch( (error) => { setSignupError(error.code) })
+  }
+
+  const SigninHandler = ( email, password ) => {
+    signInWithEmailAndPassword( FBauth, email, password )
+    .then( (userCredential) => {
+      setUser(userCredential.user)
+      setAuth(true)
+      console.log( userCredential.user.uid)
+    })
+    .catch( (error) => { 
+      const message = (error.code.includes('/') ) ? error.code.split('/')[1].replace(/-/g, ' ') : error.code
+      setSigninError(message) 
+    })
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator screenOptions={{headerShown: false}} >
+      <Stack.Screen name="Signup" options={{title: 'Sign up'}}>
+          { (props) => 
+          <Signup {...props} 
+          handler={SignupHandler} 
+          auth={auth} 
+          error={signupError} 
+          /> }
+        </Stack.Screen>
+        <Stack.Screen 
+          name="Signin" 
+          options={{
+            title:'Sign in'
+          }}
+        >
+          { (props) => 
+          <Signin {...props} 
+          auth={auth} 
+          error={signinError} 
+          handler={SigninHandler} 
+          /> }
+        </Stack.Screen>
+        <Stack.Screen name="Home" component={Home}/>
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+export default () => {
+  return (
+    <NavigationContainer>
+     
+        <App />
+      
+    </NavigationContainer>
+  )
+}
---
+++ Config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDZ65OuslX79yQA9BGpYXfzET_t9MVIA7E",
+    authDomain: "calendarioapp-c4a61.firebaseapp.com",
+    projectId: "calendarioapp-c4a61",
+    storageBucket: "calendarioapp-c4a61.appspot.com",
+    messagingSenderId: "462508599086",
+    appId: "1:462508599086:web:60389c99a90f78fb117cbf",
+    measurementId: "G-9HS23GMBRP"
+  };
